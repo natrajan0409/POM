3b649e8e7779a0462d6d7de6d208daee43a2346f
natrajan0409 <50269661+natrajan0409@users.noreply.github.com>
2019-12-14 08:16:29 +0530

    assert verification

--- src/main/java/POM/Webpagelauch.java
+++ src/main/java/POM/Webpagelauch.java
@@ -1,19 +1,46 @@
 package POM;
 
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+
+import com.google.common.io.Files;
+
 
 public class Webpagelauch {
 
-	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("Webdriver.chromer.driver","E://Selinium//chromedriver.exe");
-		WebDriver driver= new ChromeDriver();
-		driver.get("https://github.com/natrajan0409/POM");
+	public static <Takescreenshoot> void main(String[] args) throws InterruptedException, IOException {
+		System.setProperty("Webdriver.EDGE.driver","E://Selinium//msedgedriver");
+		WebDriver driver= new EdgeDriver();
+		driver.get("https://github.com/login");
 		Thread.sleep(500);
-		driver.close();
-		
-		
-		
-	}
+		driver.findElement(By.xpath("//*[@id=\"login_field\"]")).sendKeys("natrajan0409");
+		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("nat@852906");
+		driver.findElement(By.xpath("//*[@id=\"login\"]/form/div[3]/input[8]")).click();
+	String expected ="https://github.com/natrajan0409";
 
+		if(Assert.assertEquals(expected, driver.getCurrentUrl()))
+
+				{
+			System.out.print("b");
+			java.io.File src= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(src, new java.io.File("E:\\Selinium\\SCREEN.png"));
+		}
+		else 
+		{
+			driver.close();
+		}
+		
+		driver.close();
+	}
+ 
 }
